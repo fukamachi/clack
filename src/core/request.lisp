e5dc30b48206fc7b8b82eb03c31555a318b22f1a
fukamachi <e.arrows@gmail.com>
2011-02-01 17:48:31 +0900

    added `query-parameters' and `body-parameters' to clack.request for accessing parsed parameters.

--- src/core/request.lisp
+++ src/core/request.lisp
@@ -15,9 +15,31 @@
 (in-package :cl-user)
 
 (defpackage clack.request
-  (:use :cl :cl-ppcre)
+  (:use :cl
+        :cl-ppcre
+        :flexi-streams
+        :metabang-bind
+        :anaphora)
   (:export :<request>
-           :make-request))
+           :make-request
+           :request-method
+           :script-name
+           :path-info
+           :server-name
+           :server-port
+           :server-protocol
+           :request-uri
+           :uri-scheme
+           :remote-addr
+           :remote-port
+           :query-string
+           :raw-body
+           :content-length
+           :content-type
+           :clack-handler
+           :body-parameters
+           :query-parameters
+           ))
 
 (in-package :clack.request)
 
@@ -36,8 +58,60 @@
       (raw-body :initarg :raw-body :initform nil :accessor raw-body)
       (content-length :initarg :content-length :initform nil :accessor content-length)
       (content-type :initarg :content-type :initform nil :accessor content-type)
-      (clack-handler :initarg :clack-handler :initform nil :accessor clack-handler)))
+      (clack-handler :initarg :clack-handler :initform nil :accessor clack-handler)
+
+      (body-parameters :initform nil)
+      (query-parameters :initform nil)))
 
 (defun make-request (req)
   "Make a <request> instance from request plist."
   (apply #'make-instance '<request> req))
+
+;(defmethod initialize-instance :after ((req <request>) &rest initargs))
+
+(defmethod body-parameters ((req <request>))
+  (awhen (slot-value req 'body-parameters)
+    (return-from body-parameters it))
+
+  (setf (slot-value req 'body-parameters)
+        (bind ((body (raw-body req))
+               ((:values type subtype charset)
+                (parse-content-type (content-type req)))
+               (content-type (concatenate 'string type "/" subtype))
+               (external-format (flex:make-external-format (intern (string-upcase charset) :keyword) :eol-style :lf)))
+          (cond
+            ((string= content-type "application/x-www-form-urlencoded")
+             (parameters->plist (read-line body)))
+            ((string= content-type "multipart/form-data")
+             ;; FIXME: depends on Hunchentoot.
+             (hunchentoot::parse-rfc2388-form-data
+              (flex:make-flexi-stream body)
+              content-type
+              external-format)))))
+
+  (slot-value req 'body-parameters))
+
+(defmethod query-parameters ((req <request>))
+  (awhen (slot-value req 'query-parameters)
+    (return-from query-parameters it))
+
+  (setf (slot-value req 'query-parameters)
+        (parameters->plist (query-string req)))
+
+  (slot-value req 'query-parameters))
+
+(defun parameters->plist (params)
+  (loop for kv in (ppcre:split "&" params)
+        for (k v) = (ppcre:split "=" kv)
+        append (list (intern k :keyword) v)))
+
+(defun parse-content-type (content-type)
+  (register-groups-bind (type subtype params)
+      ("^(.+?)/([^;]+);?(?:(.+)|$)" content-type)
+    (values
+     (or type "application")
+     (or subtype "octet-stream")
+     (awhen params
+       (aand (nth-value 1 (ppcre:scan-to-strings
+                           "charset=([^; ]+)" params))
+             (aref it 0))))))
---
+++ t/core/request.lisp
@@ -0,0 +1,33 @@
+(in-package :cl-user)
+
+(defpackage clack-test.request
+  (:use :cl
+        :cl-test-more
+        :flexi-streams
+        :clack.request))
+
+(in-package :clack-test.request)
+
+(defvar req
+    (make-request `(:content-type "application/x-www-form-urlencoded; charset=utf-8"
+                    :query-string "ediweitz=weitzedi"
+                    :raw-body
+                    ,(flex:make-flexi-stream
+                      (flex:make-in-memory-input-stream
+                       #(110 97 109 101 61 230 183 177 231 148 186 232 139 177 229 164 170 233 131 142))
+                      :external-format :utf-8))))
+
+(plan 3)
+
+(is (content-type req)
+    "application/x-www-form-urlencoded; charset=utf-8")
+
+(is (query-parameters req)
+    '(:|ediweitz| "weitzedi"))
+
+(is (body-parameters req)
+    `(:|name| ,(flex:octets-to-string
+                #(230 183 177 231 148 186 232 139 177 229 164 170 233 131 142)
+                :external-format :utf-8)))
+
+(finalize)
