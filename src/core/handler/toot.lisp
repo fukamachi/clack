3b30aeea9e9eac7330f47bc723218a44bf823bd8
Kim Sunrim <3han5chou7@gmail.com>
2013-10-09 23:16:22 +0900

    Make most of tests pass. The rest may be toot's bug. Failed tests are here <https://gist.github.com/KeenS/6898642>

--- src/core/handler/toot.lisp
+++ src/core/handler/toot.lisp
@@ -9,25 +9,34 @@
 (clack.util:namespace clack.handler.toot
   (:use :cl
         :toot
+        :anaphora
         :split-sequence
         :ppcre)
   (:shadow :handle-request)
   (:import-from :clack.component
                 :<component>
-                :call))
+                :call)
+  (:import-from :flexi-streams
+		:octets-to-string))
 
 (cl-syntax:use-syntax :annot)
 
 @export
-(defun run (app &key (port 5000) debug)
+(defun run (app &key debug (port 5000))
   "Start Toot server."
-  (declare (ignore debug))
   (toot:start-server
    :handler (lambda (req)
               (handle-response
                req
-               (call app (handle-request req))))
+               (if debug (call app (handle-request req))
+		   (aif (handler-case (call app (handle-request req))
+			  (condition (error)
+			    @ignore error
+			    nil))
+			it
+			'(500 nil nil)))))
    :port port))
+
 
 @export
 (defun stop (acceptor)
@@ -38,14 +47,15 @@
   "Convert Request from server into a plist
 before pass to Clack application."
   (let ((content-length (and (request-header :content-length req)
-                             (parse-integer (request-header :content-length req) :junk-allowed t))))
+                             (parse-integer (request-header :content-length req) :junk-allowed t)))
+	(port-and-host (get-port-and-host req)))
     (append
      (list
       :request-method (request-method req)
       :script-name ""
-      :path-info (request-path req)
-      :server-name (request-host req)
-      :server-port (request-port req)
+      :path-info (url-decode (request-path req))
+      :server-name (car port-and-host)
+      :server-port (cdr port-and-host)
       :server-protocol (server-protocol req)
       :request-uri (request-uri req)
       :url-scheme :HTTP;(request-scheme req)
@@ -70,8 +80,8 @@ before pass to Clack application."
   (destructuring-bind (status headers body) res
     (etypecase body
       (pathname
-       (serve-file req body
-                   :content-type (getf headers :content-type)))
+       (multiple-value-call #'serve-file
+	 (values req body (parse-charset (getf headers :content-type)))))
       (list
        ;; XXX: almost same as Clack.Handler.Hunchentoot's one.
        (setf (status-code req) status)
@@ -101,3 +111,55 @@ before pass to Clack application."
 		(subseq content-type (aref reg1 1) (aref reg2 1)))
 	;; there is no ";charset="
 	(values content-type toot::*default-charset*))))
+
+(defun get-port-and-host (req)
+  (destructuring-bind (server-name &optional (server-port "80"))
+      (split-sequence #\: (cdr (assoc :host (request-headers req))))
+    (cons server-name (parse-integer server-port))))
+
+(defun url-decode (string &optional (external-format toot::*default-charset*))
+  "Decodes a URL-encoded string which is assumed to be encoded using the
+external format EXTERNAL-FORMAT, i.e. this is the inverse of
+URL-ENCODE. It is assumed that you'll rarely need this function, if
+ever. But just in case - here it is. The default for EXTERNAL-FORMAT is
+the value of *default-charset*."
+  (when (zerop (length string))
+    (return-from url-decode ""))
+  (let ((vector (make-array (length string) :element-type 'octet :fill-pointer 0))
+        (i 0)
+        unicodep)
+    (loop
+      (unless (< i (length string))
+        (return))
+      (let ((char (aref string i)))
+       (labels ((decode-hex (length)
+                  (prog1
+                      (parse-integer string :start i :end (+ i length) :radix 16)
+                    (incf i length)))
+                (push-integer (integer)
+                  (vector-push integer vector))
+                (peek ()
+                  (aref string i))
+                (advance ()
+                  (setq char (peek))
+                  (incf i)))
+         (cond
+          ((char= #\% char)
+           (advance)
+           (cond
+            ((char= #\u (peek))
+             (unless unicodep
+               (setq unicodep t)
+               (upgrade-vector vector '(integer 0 65535)))
+             (advance)
+             (push-integer (decode-hex 4)))
+            (t
+             (push-integer (decode-hex 2)))))
+          (t
+           (push-integer (char-code (case char
+                                      ((#\+) #\Space)
+                                      (otherwise char))))
+           (advance))))))
+    (cond (unicodep
+           (upgrade-vector vector 'character :converter #'code-char))
+          (t (octets-to-string vector :external-format external-format)))))
