fa753c35a65d76553d5513ee5218de3536898e7e
Eitaro Fukamachi <e.arrows@gmail.com>
2015-01-09 03:58:02 +0900

    Change the port number for Clack.Handler.Fcgi tests.

--- t/core/handler/fcgi.lisp
+++ t/core/handler/fcgi.lisp
@@ -4,6 +4,6 @@
         :clack.test.suite))
 (in-package :clack-test.handler.fcgi)
 
-(let ((clack.test:*clack-test-port* 14242)
-      (clack.test.suite:*clack-test-access-port* 4242))
+(let ((clack.test:*clack-test-port* 14949)
+      (clack.test.suite:*clack-test-access-port* 4949))
   (clack.test.suite:run-server-tests :fcgi))
