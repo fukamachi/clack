cc5ce12fda63bb17533be5b025049538da4587ca
Eitaro Fukamachi <e.arrows@gmail.com>
2015-01-08 23:51:37 +0900

    Add plan, skip and finalize for preventing empty test-files.

--- t/core/handler/toot.lisp
+++ t/core/handler/toot.lisp
@@ -2,6 +2,7 @@
 
 (defpackage clack-test.handler.toot
   (:use :cl
+        :prove
         :clack.test.suite))
 
 (in-package :clack-test.handler.toot)
@@ -9,3 +10,8 @@
 #+thread-support
 (let ((*error-output* (make-broadcast-stream)))
   (clack.test.suite:run-server-tests :toot))
+#-thread-support
+(progn
+  (plan 1)
+  (skip 1 "because your Lisp doesn't support threads")
+  (finalize))
--- t/core/handler/wookie.lisp
+++ t/core/handler/wookie.lisp
@@ -1,9 +1,15 @@
 (in-package :cl-user)
 (defpackage clack-test.handler.wookie
   (:use :cl
+        :prove
         :clack.test.suite))
 (in-package :clack-test.handler.wookie)
 
 #+thread-support
 (let ((*error-output* (make-broadcast-stream)))
   (clack.test.suite:run-server-tests :wookie))
+#-thread-support
+(progn
+  (plan 1)
+  (skip 1 "because your Lisp doesn't support threads")
+  (finalize))
