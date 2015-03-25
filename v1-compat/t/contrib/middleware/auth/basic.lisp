696cfde287521b4be2b64e8256bce41870fdc5ec
Eitaro Fukamachi <e.arrows@gmail.com>
2015-03-25 21:14:07 +0900

    Add t-clack-v1-compat.

--- v1-compat/t/contrib/middleware/auth/basic.lisp
+++ v1-compat/t/contrib/middleware/auth/basic.lisp
@@ -11,27 +11,26 @@
 (plan 4)
 
 #+thread-support
-(test-app
- (builder
-  (<clack-middleware-auth-basic>
-   :authenticator
-   #'(lambda (user pass)
-       (and (string= user "hoge")
-            (string= pass "fuga"))))
-  (lambda (env)
-    `(200 nil (,(format nil "Hello, ~A" (getf env :remote-user))))))
- (lambda ()
-   (is (http-request "http://localhost:4242/")
-       "Authorization required")
-   (is (http-request "http://localhost:4242/"
-                     :basic-authorization '("" ""))
-       "Authorization required")
-   (is (http-request "http://localhost:4242/"
-                     :basic-authorization '("wrong" "auth"))
-       "Authorization required")
-   (is (http-request "http://localhost:4242/"
-                     :basic-authorization '("hoge" "fuga"))
-       "Hello, hoge")))
+(subtest-app "auth basic"
+    (builder
+     (<clack-middleware-auth-basic>
+      :authenticator
+      #'(lambda (user pass)
+          (and (string= user "hoge")
+               (string= pass "fuga"))))
+     (lambda (env)
+       `(200 nil (,(format nil "Hello, ~A" (getf env :remote-user))))))
+  (is (http-request "http://localhost:4242/")
+      "Authorization required")
+  (is (http-request "http://localhost:4242/"
+                    :basic-authorization '("" ""))
+      "Authorization required")
+  (is (http-request "http://localhost:4242/"
+                    :basic-authorization '("wrong" "auth"))
+      "Authorization required")
+  (is (http-request "http://localhost:4242/"
+                    :basic-authorization '("hoge" "fuga"))
+      "Hello, hoge"))
 
 #-thread-support
 (skip 4 "because your lisp doesn't support threads")
