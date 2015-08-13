699259856f46a7e0a115874add22a4ae7a36b8de
Eitaro Fukamachi <e.arrows@gmail.com>
2015-08-13 10:15:42 +0900

    Add write-to-socket-buffer & flush-socket-buffer to clack.socket API for asynchronous sending.

--- src/socket.lisp
+++ src/socket.lisp
@@ -2,9 +2,20 @@
 (defpackage clack.socket
   (:use :cl)
   (:export :set-read-callback
-           :write-to-socket))
+           :write-to-socket
+           :write-to-socket-buffer
+           :flush-socket-buffer))
 (in-package :clack.socket)
 
 (defgeneric set-read-callback (socket callback))
 
 (defgeneric write-to-socket (socket message &key callback))
+
+(defgeneric write-to-socket-buffer (socket message)
+  (:method (socket message)
+    (write-to-socket socket message)))
+
+(defgeneric flush-socket-buffer (socket &key callback)
+  (:method (socket &key callback)
+    (declare (ignore socket callback))
+    nil))
