52f15f6c459ee6a2f3c6c64105eaf4242f08b4ee
Eitaro Fukamachi <e.arrows@gmail.com>
2016-12-02 13:10:45 +0900

    Delete clack-v1-compat.

--- v1-compat/src/contrib/test.lisp
+++
@@ -1,5 +0,0 @@
-(in-package :clack.test)
-
-(defun test-app (app client &optional desc)
-  `(subtest-app ,desc ,app (funcall ,client)))
-(export 'test-app)
--- v1-compat/src/core/builder.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :cl-user)
-(defpackage clack.builder
-  (:use :cl)
-  (:import-from :lack
-                :builder)
-  (:export :builder))
-(in-package :clack.builder)
