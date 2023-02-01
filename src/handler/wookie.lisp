fukamachi/clack

commit d63286d3cb62907ebc0744a671d8b4cbcdb9c9b5

wookie: Fix POST handling
POSTing data without this patch results in http-body.util:slurp-stream calling make-array with a string dimensions argument, which it responds to with a very cryptic warning:
   The value
     "464"
   is not of type
     (OR LIST (MOD 4611686018427387901))
   when binding SB-VM::DIMENSIONS

src/handler/wookie.lisp
@@ -114,7 +114,7 @@
             :url-scheme (if ssl "https" "http")
             :request-uri (request-resource req)
             :raw-body (flex:make-in-memory-input-stream (wookie:request-body req))
-            :content-length (gethash "content-length" headers)
+            :content-length (parse-integer (gethash "content-length" headers))
             :content-type (gethash "content-type" headers)
             :clack.streaming t
             :clack.nonblocking t

commit 4f33ce185f9f782cc64e118040be6c250a2609e3

wookie: Fix GET handling
Regression caused by d63286d .

src/handler/wookie.lisp
@@ -93,9 +93,10 @@
       (as:socket-closed () nil))))
 
 (defun handle-request (req &key ssl)
-  (let ((quri (request-uri req))
-        (http-version (http-version (request-http req)))
-        (headers (request-headers req)))
+  (let* ((quri (request-uri req))
+         (http-version (http-version (request-http req)))
+         (headers (request-headers req))
+         (content-length (gethash "content-length" headers)))
 
     (destructuring-bind (server-name &optional server-port)
         (split-sequence #\: (gethash "host" headers "") :from-end t :count 2)
@@ -114,7 +115,8 @@
             :url-scheme (if ssl "https" "http")
             :request-uri (request-resource req)
             :raw-body (flex:make-in-memory-input-stream (wookie:request-body req))
-            :content-length (parse-integer (gethash "content-length" headers))
+            :content-length (when content-length
+                              (parse-integer content-length :junk-allowed t))
             :content-type (gethash "content-type" headers)
             :clack.streaming t
             :clack.nonblocking t